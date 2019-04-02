a5f8535276fb576e9c29e54e57e8e022bc02544d
David Raygoza Gómez <raygoza.gomez@gmail.com>
2019-04-02 17:31:15 -0600

    V1.6 Se agrego funcionalidad en DisplayMessageActivity

--- app/src/main/java/com/example/app1/DisplayMessageActivity.java
+++ app/src/main/java/com/example/app1/DisplayMessageActivity.java
@@ -1,11 +1,10 @@
 package com.example.app1;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -13,17 +12,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 
 }

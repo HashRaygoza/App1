a615b220295931e394fa9d6c79d7d0e2fe3f40f6
David Raygoza Gómez <raygoza.gomez@gmail.com>
2019-04-02 17:02:36 -0600

    V1.5 Se creo la actividad que se mostrara al presionar el boton, aun no hace nada

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.app1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.app1.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
